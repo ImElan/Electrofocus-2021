dbd6c8316317794803465e7a644d6dc87e3416af
ImElan <elansakthivel2000@gmail.com>
2020-06-08 17:41:34 +0530

    install react-router and setup basic routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Router,Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import WorkShop from './Pages/WorkShop';
+import Event from './Pages/Event';
 import './sass/main.scss';
 
 function App() {
   return (
       <div>
-           <Home /> 
+            <Switch>
+                  <Route 
+                        exact
+                        path='/'
+                        render= {() => <Home />}
+                  />
+                  <Route 
+                        exact
+                        path='/workshops'
+                        render= {() =>  <WorkShop />}
+                  />
+                  <Route 
+                        exact
+                        path='/events'
+                        render= {() => <Event /> }
+                  />
+                  <Route 
+                        exact
+                        render= {() => <h1>404 Page not found..</h1> }
+                  />
+            </Switch>
       </div>
   );
 }
---
+++ src/Pages/WorkShop.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class WorkShop extends Component {
+      render() {
+            return(
+                  <div>
+                        <h1>WorkShops Page</h1>
+                  </div>
+            )
+      }
+}
+
+export default WorkShop;
