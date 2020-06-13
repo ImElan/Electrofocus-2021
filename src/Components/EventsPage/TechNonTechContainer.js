785dacf9a0c893bf4f55ad0836407a5669e7e52b
ImElan <elansakthivel2000@gmail.com>
2020-06-13 23:00:24 +0530

    add route to tech and non tech events list page

--- src/Components/EventsPage/TechNonTechContainer.js
+++ src/Components/EventsPage/TechNonTechContainer.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react';
 
+import { NavLink } from 'react-router-dom';
+
 import tech from '../../assets/tech.svg';
 import nonTech from '../../assets/nonTech.svg';
 
@@ -7,22 +9,30 @@ class TechNonTechContainer extends Component {
       render() {
             return(
                   <div className='techNonTechEvents'>  
-                        <div className='techNonTechEvents__nonTechImageContainer'>
+                        <NavLink 
+                              exact
+                              to='/nonTechEvents'
+                              className='techNonTechEvents__nonTechImageContainer'
+                        >
                               <img className='techNonTechEvents__nonTechImage' src={nonTech} alt='tech'></img>
                               <h1 
                                     className='heading--1 heading--1-dark text-center techNonTechEvents__heading'
                               >
                                     Non-Tech Events
                               </h1>
-                        </div>
-                        <div className='techNonTechEvents__techImageContainer'>
+                        </NavLink>
+                        <NavLink 
+                              exact
+                              to='/techEvents'
+                              className='techNonTechEvents__techImageContainer'
+                        >
                               <img className='techNonTechEvents__techImage' src={tech} alt='non tech'></img>
                               <h1 
                                     className='heading--1 heading--1-dark text-center techNonTechEvents__heading'
                               >
                                     Tech Events
                               </h1>
-                        </div>
+                        </NavLink>
                   </div>
             )
       }
