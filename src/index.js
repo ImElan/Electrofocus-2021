a98b8da15e17d500f900215dfb6676338211f338
ImElan <elansakthivel2000@gmail.com>
2020-06-16 13:35:02 +0530

    move scroll to top into a seperate folder in components (-----> HelperComponents Folder <----)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import ScrollToTop from './ScrollToTop';
+import ScrollToTop from './Components/HelperComponents/ScrollToTop';
 
 ReactDOM.render(
       <React.StrictMode>
